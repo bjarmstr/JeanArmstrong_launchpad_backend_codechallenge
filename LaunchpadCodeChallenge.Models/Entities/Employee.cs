e320e8986c47217e6b68527df793c24d5adf819c
Jean Armstrong <bjarmstr@yahoo.com>
2021-12-11 13:57:21 -0700

    added comments to Employee

--- LaunchpadCodeChallenge.Models/Entities/Employee.cs
+++ LaunchpadCodeChallenge.Models/Entities/Employee.cs
@@ -9,19 +9,40 @@ namespace LaunchpadCodeChallenge.Models.Entities
 {
     public class Employee
     {
+        /// <summary>
+        /// Unique identifier - Guid 
+        /// </summary>
         [Key]
         public Guid Id { get; set; }
 
+        /// <summary>
+        /// First Name of Employee
+        /// </summary>
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// Last Name of Employee
+        /// </summary>
         public string LastName { get; set; }
 
+        /// <summary>
+        /// Job Title for Employee
+        /// </summary>
         public string JobTitle { get; set; }
 
+        /// <summary>
+        /// Mailing Address
+        /// </summary>
         public string Address { get; set; }
 
+        /// <summary>
+        /// Department that employee is assigned to
+        /// </summary>
         public string DepartmentId { get; set; }
-        
+
+        /// <summary>
+        /// Department is navigation property, which allows access to Department details
+        /// </summary>
         public Department Department { get; set; }
 
 
