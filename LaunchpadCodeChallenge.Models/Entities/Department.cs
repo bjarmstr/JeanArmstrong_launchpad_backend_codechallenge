8c2c0eb870cb9d4c831ca40a46f029a9478eaf8e
Jean Armstrong <bjarmstr@yahoo.com>
2021-12-11 11:32:58 -0700

    added department and employees entities

---
+++ LaunchpadCodeChallenge.Models/Entities/Department.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaunchpadCodeChallenge.Models.Entities
+{
+    public class Department
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Address is unique
+        /// </summary>
+        [Key]
+        public string Address { get; set; }
+    }
+}
