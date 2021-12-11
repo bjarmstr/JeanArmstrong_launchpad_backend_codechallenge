7c9229fc761bf9885dd0027ed333115ddda08899
Jean Armstrong <bjarmstr@yahoo.com>
2021-12-11 13:47:46 -0700

    added comments to Entity

--- LaunchpadCodeChallenge.Models/Entities/Department.cs
+++ LaunchpadCodeChallenge.Models/Entities/Department.cs
@@ -9,17 +9,27 @@ namespace LaunchpadCodeChallenge.Models.Entities
 {
     public class Department
     {
+        /// <summary>
+        /// Guid Identifier - Uniquely Identifies the Department
+        /// </summary>
         [Key]
         public Guid Id { get; set; }
 
+        /// <summary>
+        /// Department Name
+        /// </summary>
         public string Name { get; set; }
 
         /// <summary>
-        /// Address is unique
+        /// Each Department has a Unique Address
         /// </summary>
         [Key]
+        [Required]
         public string Address { get; set; }
 
+        /// <summary>
+        /// Employees in the Department
+        /// </summary>
         public List<Employee> Employees { get; set; }
     }
 }
