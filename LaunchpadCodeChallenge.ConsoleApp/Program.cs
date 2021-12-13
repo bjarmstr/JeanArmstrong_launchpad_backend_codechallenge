24272525db6175d04e282daa370c6bd2fe388249
Jean Armstrong <bjarmstr@yahoo.com>
2021-12-12 20:44:19 -0700

    switch statement by type

--- LaunchpadCodeChallenge.ConsoleApp/Program.cs
+++ LaunchpadCodeChallenge.ConsoleApp/Program.cs
@@ -25,8 +25,12 @@ namespace LaunchpadCodeChallenge.ConsoleApp
             TestModule(ref myvar);
 
             //case b.  integer >4
+            myvar = 6;
+            TestModule(ref myvar);
 
             //case c. integer < 1
+            myvar = -4;
+            TestModule(ref myvar);
 
             //case d.  float 1f and 2f
             float myfloat = 1.0f;
@@ -40,9 +44,10 @@ namespace LaunchpadCodeChallenge.ConsoleApp
             //Test fails this case
             DateTime thisDate1 = new DateTime(2011, 6, 10);
             var valueTypeDT = thisDate1.GetType();
-            
+            Console.WriteLine($"{thisDate1} of type {valueTypeDT}");
+            TestModule(ref thisDate1);
             bool b = true;
-           // TestModule(ref thisDate1);
+            TestModule(ref b);
 
 
         }
@@ -55,8 +60,28 @@ namespace LaunchpadCodeChallenge.ConsoleApp
 
             switch (value)
             {
-                case (int i ) :
-                    Console.WriteLine($"integer {i}");
+                case int i  :
+                    if (i >= 1 && i <= 4)
+                    {
+                        var quotient = i * 2;
+                        //return i
+                        Console.WriteLine($"multiplied by 2 {quotient}");
+                        break;
+                    }
+                    if (i > 4)
+                    {
+                        var quotient = i * 3;
+                        
+                        //return i
+                        Console.WriteLine($"multiplied by 3 {quotient}");
+                    }
+                    if (i < 1)
+                    {
+
+                        throw new ArgumentException(
+                            String.Format("{0} is an incorrect input, integer must be >= 1");
+                    }
+                    
                     break;
 
                 case float t:
@@ -68,7 +93,7 @@ namespace LaunchpadCodeChallenge.ConsoleApp
                     break;
 
                 default:
-                    Console.WriteLine($"{value} of type {valueType}");
+                    Console.WriteLine($"default");
                     break;
             }
 
