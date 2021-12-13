6eb060d03aced79a5f746ba33719b5c40ae710c7
Jean Armstrong <bjarmstr@yahoo.com>
2021-12-12 17:21:35 -0700

    create consoleApp take2

---
+++ LaunchpadCodeChallenge.ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace LaunchpadCodeChallenge.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("test");
+            List<string>.Enumerator iterateNames = QuestionClass.CreateNamesListEnumerator();
+            while (iterateNames.MoveNext())
+            {
+                string str = iterateNames.Current;
+                Console.WriteLine(str);
+            }
+
+        }
+
+
+    }
+}
+
---
+++ LaunchpadCodeChallenge.ConsoleApp/QuestionClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaunchpadCodeChallenge.ConsoleApp
+{
+    public static class QuestionClass
+    {
+
+
+        static List<string> NamesList = new List<string>()
+            {
+                "Jimmy",
+                "Jeffrey",
+                "John",
+            };
+
+        public static List<string>.Enumerator CreateNamesListEnumerator()
+        {
+
+            //GetEnumerator Returns an Enumerator that Iterates through the List
+            List<string>.Enumerator enumeratorNamesList = NamesList.GetEnumerator();
+
+            return enumeratorNamesList;
+
+        }
+
+    }
+}
