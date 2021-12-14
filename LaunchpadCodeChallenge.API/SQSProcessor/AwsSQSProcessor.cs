c3f69aef203b39e209bf4f8b9f90de711e8c2fc2
Jean Armstrong <bjarmstr@yahoo.com>
2021-12-13 21:49:38 -0700

    using AWS SQS library instead of HTTP

---
+++ LaunchpadCodeChallenge.API/SQSProcessor/AwsSQSProcessor.cs
@@ -0,0 +1,34 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LaunchpadCodeChallenge.API.SQSProcessor
+{
+    public class AwsSQSProcessor
+    {
+
+        public string readQueue()
+        {
+            //uses credentials in .aws folder on local computer
+            
+            var sqs = new AmazonSQSClient(RegionEndpoint.CACentral1);
+
+            var queueUrl = sqs.GetQueueUrlAsync("DynamoQueue").Result.QueueUrl;
+
+            var receiveMessageRequest = new ReceiveMessageRequest
+            {
+                QueueUrl = queueUrl
+            };
+
+            var receiveMessageResponse = sqs.ReceiveMessageAsync(receiveMessageRequest).Result;
+
+
+
+        }    
+    }
+}
+
